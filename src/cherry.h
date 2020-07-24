ba6ccb78d603952a42772f4273b7078fc96d485d
Sam Pritchard <ophisbonklol@gmail.com>
2020-07-24 21:44:21 +0100

    Add include guard for cherry.h

--- src/cherry.h
+++ src/cherry.h
@@ -1,10 +1,13 @@
-#include <xcb/xcb.h>
-#include <lua.h>
+#ifndef CHERRY_H
+    #define CHERRY_H
+    #include <xcb/xcb.h>
+    #include <lua.h>
 
-typedef struct cherry_state_t {
-    int keep_running;
-    xcb_connection_t *connection;
-    const xcb_setup_t *setup;
-    xcb_screen_t *screen;
-    lua_State *lua_state;
-} cherry_state_t;
+    typedef struct cherry_state_t {
+        int keep_running;
+        xcb_connection_t *connection;
+        const xcb_setup_t *setup;
+        xcb_screen_t *screen;
+        lua_State *lua_state;
+    } cherry_state_t;
+#endif
