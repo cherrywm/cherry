adf9b03e0e81032d873809450777bf2f71e3e401
Sam Pritchard <ophisbonklol@gmail.com>
2020-07-25 20:24:18 +0100

    Update Lua example

--- examples/cherry.lua
+++ examples/cherry.lua
@@ -1,6 +1,12 @@
 -- Note: These values all have defaults. For most users, an empty configuration file will do.
 fifo_location = "/tmp/cherry"
 desktop_count = 1
+border_width = 1
+window_gap = 20
+top_padding = 10
+bottom_padding = 10
+right_padding = 10
+left_padding = 10
 
 -- This is a Lua script! We can take full advantage of Lua here.
 -- Later versions of cherry will provide an API, so that cherry extensions can be written.
