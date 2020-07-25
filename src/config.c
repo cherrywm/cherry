36167c2f481d21c4472904ce3792d7087e7f8950
Sam Pritchard <ophisbonklol@gmail.com>
2020-07-25 20:22:12 +0100

    Add more config options.

--- src/config.c
+++ src/config.c
@@ -23,11 +23,36 @@ cherry_config_t* run_config_file(lua_State *lua_state, const char *file_location
     // Note that in the case of missing data, defaults are provided.
 
     cherry_config_t *config = (cherry_config_t*) malloc(sizeof(cherry_config_t));
+    
     config->lua_state = lua_state;
-    config->fifo_path = lua_isnoneornil(lua_state, -2) ? CHERRY_DEFAULT_FIFO_LOCATION : lua_tostring(lua_state, -2);
-    config->desktop_count = lua_isnoneornil(lua_state, -1) ? 1 : lua_tonumber(lua_state, -1);
+    config->fifo_path = string_from_config(lua_state, "fifo_location", CHERRY_DEFAULT_FIFO_LOCATION);
+    config->desktop_count = int_from_config(lua_state, "desktop_count", CHERRY_DEFAULT_DESKTOP_COUNT);
+    config->border_width = int_from_config(lua_state, "border_width", CHERRY_DEFAULT_BORDER_WIDTH);
+    config->window_gap = int_from_config(lua_state, "window_gap", CHERRY_DEFAULT_WINDOW_GAP);
+    config->top_padding = int_from_config(lua_state, "top_padding", CHERRY_DEFAULT_TOP_PADDING);
+    config->bottom_padding = int_from_config(lua_state, "bottom_padding", CHERRY_DEFAULT_BOTTOM_PADDING);
+    config->right_padding = int_from_config(lua_state, "right_padding", CHERRY_DEFAULT_RIGHT_PADDING);
+    config->left_padding = int_from_config(lua_state, "left_padding", CHERRY_DEFAULT_LEFT_PADDING);
 
     return config;
+}
+
+int int_from_config(lua_State *lua_state, const char *key, int default_value) {
+    lua_getglobal(lua_state, key);
+    
+    return lua_isnoneornil(lua_state, -1) ? default_value : lua_tonumber(lua_state, -1);
+}
+
+int boolean_from_config(lua_State *lua_state, const char *key, int default_value) {
+    lua_getglobal(lua_state, key);
+    
+    return lua_isnoneornil(lua_state, -1) ? default_value : lua_toboolean(lua_state, -1);
+}
+
+const char* string_from_config(lua_State *lua_state, const char *key, const char *default_value) {
+    lua_getglobal(lua_state, key);
+    
+    return lua_isnoneornil(lua_state, -1) ? default_value : lua_tostring(lua_state, -1);
 }
 
 // Exits application if the file does not exist.
--- src/config.h
+++ src/config.h
@@ -1,6 +1,14 @@
 #ifndef CHERRY_CONFIGH
     #define CHERRY_CONFIGH
+
     #define CHERRY_DEFAULT_FIFO_LOCATION "/tmp/cherry"
+    #define CHERRY_DEFAULT_DESKTOP_COUNT 1
+    #define CHERRY_DEFAULT_BORDER_WIDTH 1
+    #define CHERRY_DEFAULT_WINDOW_GAP 20
+    #define CHERRY_DEFAULT_TOP_PADDING 10
+    #define CHERRY_DEFAULT_BOTTOM_PADDING 10
+    #define CHERRY_DEFAULT_RIGHT_PADDING 10
+    #define CHERRY_DEFAULT_LEFT_PADDING 10
 
     #include <stdio.h>
     #include <stdlib.h>
@@ -15,10 +23,21 @@
         lua_State *lua_state;
         const char *fifo_path;
         int desktop_count;
+        int border_width;
+        int window_gap;
+        int top_padding;
+        int bottom_padding;
+        int right_padding;
+        int left_padding;
     } cherry_config_t;
 
     // Assumes Lua has already been setup.
     cherry_config_t* run_config_file(lua_State *lua_state, const char *file_location);
+
+    // Helper functions.
+    int int_from_config(lua_State *lua_state, const char *key, int default_value);
+    int boolean_from_config(lua_State *lua_state, const char *key, int default_value);
+    const char* string_from_config(lua_State *lua_state, const char *key, const char *default_value);
 
     // Defaults to $XDG_CONFIG_HOME/cherry/cherry.lua, or $HOME/.config/cherry/cherry.lua
     const char* default_config_file_location(void);
