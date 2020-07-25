f4000b9875a1cfaf8074b3bab4b2b150f1f92880
Sam Pritchard <ophisbonklol@gmail.com>
2020-07-25 16:25:05 +0100

    reformat program

--- src/cherry.c
+++ src/cherry.c
@@ -3,6 +3,7 @@
 #include <getopt.h>
 
 #include "loop.h"
+#include "config.h"
 #include <lauxlib.h>
 #include <lualib.h>
 #include <string.h>
@@ -15,59 +16,58 @@ void stop_running(int _unused) {
     (void)(_unused);
 }
 
-// Executed if no options are passed to cherry.
-// No --config/-c option makes cherry assume a config file location of $XDG_CONFIG_HOME/cherry/cherry.lua.
-void setup() {
-    // skeleton.
-}
+void setup(const char *config_file_location) {
+    lua_State *lua_state = luaL_newstate();
+    luaL_openlibs(lua_state);
 
-int main(int argc, char *argv[]) {
-    int option_index = getopt(argc, argv, "c:");
-    if (option_index == -1) {
-        exit(1); // skeleton.
+    cherry_config_t *config = run_config_file(lua_state, config_file_location);
+    xcb_connection_t *connection = xcb_connect(NULL, NULL);
+
+    if (xcb_connection_has_error(connection)) {
+        fputs("Failed to connect to X.", stderr);
+        exit(EXIT_FAILURE);
     }
 
-    getopt(argc, argv, "c::");
-
-    // Establish connection to DISPLAY X server, first available screen.
-    // TODO: Support multple monitors.
-    xcb_connection_t *connection = xcb_connect(NULL, NULL);
+    const xcb_setup_t *setup = xcb_get_setup(connection);
+    xcb_screen_t *screen = xcb_setup_roots_iterator(setup).data;
     
-    // Check if we failed to establish a connection to the specified X server.
-    if (xcb_connection_has_error(connection)) {
-        // TODO: syslog possibly?
-        fputs("Failure to open display!", stderr);
+    uint32_t substructure_mask[1] = {
+        XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
+        XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
+    };
+
+    xcb_change_window_attributes(connection, screen->root, XCB_CW_EVENT_MASK, substructure_mask);
+    xcb_flush(connection);
+
+    if (signal(SIGTERM, stop_running) == SIG_ERR) {
+        fputs("failed to set SIGTERM handler.", stderr);
         return EXIT_FAILURE;
     }
 
-    const xcb_setup_t *setup = xcb_get_setup(connection); // Display environment properties.
-    xcb_screen_t *screen = xcb_setup_roots_iterator(setup).data; // First available screen.
-    lua_State *lua_state = luaL_newstate(); // Lua state. Used for configuration and scripting.
-
-    luaL_openlibs(lua_state);
-
-    // We're not going to be reading before writing, so malloc instead of calloc.
     state = (cherry_state_t*) malloc(sizeof(cherry_state_t));
     state->connection = connection;
     state->screen = screen;
     state->setup = setup;
     state->lua_state = lua_state;
 
-    uint32_t mask[1] = {
-        XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
-        XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY
-    };
-
-    // Sends substructure redirect.
-    xcb_change_window_attributes(connection, screen->root, XCB_CW_EVENT_MASK, mask);
-    xcb_flush(connection);
-
-    // Attempt to set a SIGTERM handler (so we can gracefully exit).
-    if (signal(SIGTERM, stop_running) == SIG_ERR) {
-        fputs("failed to set SIGTERM handler.", stderr);
-        return EXIT_FAILURE;
-    }
-
     // Starts loop. Keeps WM running until killed.
     start_loop(state);
+}
+
+int main(int argc, char *argv[]) {
+    int option = getopt(argc, argv, "c:");
+    const char *config_file_location;
+
+    switch (option) {
+        case 'c':
+            config_file_location = optarg;
+            break;
+        case '?':
+            fputs("You must specify a file location for the -c option.", stderr);
+            exit(EXIT_FAILURE); // No break; as this exits application already.
+        default:
+            config_file_location = default_config_file_location(); // No break; as this is the last case.
+    };
+
+    setup(config_file_location);
 }
