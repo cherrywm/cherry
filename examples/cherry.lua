1c28801066008864fbbf23aaed4e0bb735c57562
Sam Pritchard <ophisbonklol@gmail.com>
2020-07-25 16:58:43 +0100

    Example Lua change.

--- examples/cherry.lua
+++ examples/cherry.lua
@@ -1,3 +1,9 @@
 -- Note: These values all have defaults. For most users, an empty configuration file will do.
 fifo_location = "/tmp/cherry"
 desktop_count = 1
+
+-- This is a Lua script! We can take full advantage of Lua here.
+-- Later versions of cherry will provide an API, so that cherry extensions can be written.
+print("Hello, world!")
+
+print("The value of fifo_location is: " .. fifo_location)
